80c941f82dc4e758d1f1d961ca1a60c413feec25
jhawk7 <13038876+jhawk7@users.noreply.github.com>
2025-04-02 15:28:43 -0400

    updates to sidepanel copy button and handler

--- frontend/src/components/SidePanel.js
+++ frontend/src/components/SidePanel.js
@@ -2,6 +2,16 @@ import { useState } from "react"
 import Paste from "./Paste"
 
 const SidePanel = ({pastedata, deleteHandler}) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopy = (text) => {
+    navigator.clipboard.writeText(text); //only works for https and localhost
+    setIsCopied(true)
+
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000)
+  }
 
   return (
     <div>
@@ -9,7 +19,9 @@ const SidePanel = ({pastedata, deleteHandler}) => {
         {pastedata.map((paste, index) => (
           <div>
             <Paste paste={paste} key={index}></Paste>
-            <button>Copy</button>
+            { isCopied ? <button>check</button> :
+              <button onClick={() => handleCopy(paste.value)}>Copy</button>
+            }
             <button onClick={() => deleteHandler(paste.key)}>X</button>
           </div>
         ))}
